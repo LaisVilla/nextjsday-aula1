e1605d8aea6d0fe0461ec96261360cfacdf816cc
LaisVilla <lvsarj@yahoo.com.br>
2025-04-09 20:54:56 -0300

    feat: add new header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Card, CardHeader, CardTitle } from "./_components/ui/card";
+import { Header } from "./_components/header";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card>
-        <CardHeader>
-          <CardTitle>Conteúdo</CardTitle>
-        </CardHeader>
-      </Card>
-    </div>
+  <div className="flex items-center justify-center h-screen">
+    <Header/>
+  </div>
+   
   );
 }
